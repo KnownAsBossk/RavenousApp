d240790cf710b19cdce6f96612478a501863c9a4
kuolun <kuolun@gmail.com>
2017-09-12 11:05:56 +0800

    add searchYelp method and send to SearchBar comp

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -2,31 +2,78 @@ import React from 'react';
 import './SearchBar.css';
 
 
-let sortByOptions = {
-    'Best Match': 'best_match',
-    'Highest Rated': 'rating',
-    'Most Reviewed': 'review_count'
-};
 
 class SearchBar extends React.Component {
+    constructor(){
+        super();
+        this.state={
+            term:'',
+            location:'',
+            sortBy:'best_match'
+        };
+        this.sortByOptions={
+            'Best Match': 'best_match',
+            'Highest Rated': 'rating',
+            'Most Reviewed': 'review_count'
+        };
+        
+        //bind this to methods
+        this.getSortByClass=this.getSortByClass.bind(this);
+        this.handleSortByChange=this.handleSortByChange.bind(this);
+        this.handleTermChange=this.handleTermChange.bind(this);
+        this.handleLocationChange=this.handleLocationChange.bind(this);
+    }
+
+    //取得sorting option 的class
+    getSortByClass(sortByOption){
+        if(this.state.sortBy === sortByOption){
+            return 'active';
+        }else{
+            return '';
+        }
+    }
+
+    // 設定sort option state
+    handleSortByChange(sortByOption){
+        this.setState({
+            sortBy:sortByOption
+        })
+    }
+
+    handleTermChange(e){
+        this.setState({term:e.target.value});
+    }
+
+    handleLocationChange(e){
+        this.setState({location:e.target.value});
+    }
+
+    //把sort options render出來
     renderSortByOptions(sortByOptions) {
         return Object.keys(sortByOptions).map(sortByOption => {
             let sortByOptionValue = sortByOptions[sortByOption];
-            return <li key={sortByOptionValue}>{sortByOption}</li>
+            return <li  onClick={this.handleSortByChange(sortByOptionValue)}
+                        className={this.getSortByClass(sortByOptionValue)}
+                        key={sortByOptionValue}>{sortByOption}</li>
         });
     }
 
+    // SearchBar component render出
+    //1.search option
+    //2.search term
+    //3.search location
+    //4.search button
     render() {
         return (
             <div className="SearchBar">
                 <div className="SearchBar-sort-options">
                     <ul>
-                        {this.renderSortByOptions(sortByOptions)}
+                        {this.renderSortByOptions(this.sortByOptions)}
                     </ul>
                 </div>
                 <div className="SearchBar-fields">
-                    <input placeholder="Search Businesses" />
-                    <input placeholder="Where?" />
+                    <input onChange={this.handleTermChange} placeholder="Search Businesses" />
+                    <input onChange={this.handleLocationChange} placeholder="Where?" />
                 </div>
                 <div className="SearchBar-submit">
                     <a>Let's Go</a>
